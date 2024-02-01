36b496d68db2d7e5fbd21ae069847999b31ec130
PierluigiTornesi <pierluigi.tornesi02@gmail.com>
2024-02-01 16:03:22 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+
+export default{
+  data(){
+    return{
+
+    }
+  }
+}
+
+</script>
+
+<template>
+  <h1>Titolo di prova</h1>
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
